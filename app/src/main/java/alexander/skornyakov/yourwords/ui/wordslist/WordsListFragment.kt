d1471d66abe77e5754f7aa974687a70ec9bea441
Alexander Skornyakov <a1212sk@gmail.com>
2019-09-16 15:42:58 +0300

    wordsListViewModel initializing

--- app/src/main/java/alexander/skornyakov/yourwords/ui/wordslist/WordsListFragment.kt
+++ app/src/main/java/alexander/skornyakov/yourwords/ui/wordslist/WordsListFragment.kt
@@ -1,10 +1,12 @@
 package alexander.skornyakov.yourwords.ui.wordslist
 
 import alexander.skornyakov.yourwords.R
+import alexander.skornyakov.yourwords.databinding.WordslistFragmentBinding
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 
@@ -20,13 +22,17 @@ class WordsListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.wordslist_fragment, container, false)
+        val binding : WordslistFragmentBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.wordslist_fragment,
+            container,
+            false)
+        viewModel = ViewModelProviders.of(this).get(WordsListViewModel::class.java)
+        binding.wordsListViewModel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(WordsListViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+
 
 }
