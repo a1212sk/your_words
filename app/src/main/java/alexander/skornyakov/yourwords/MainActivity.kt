4f277cc8459efb14ca337f1289e69495bb08c105
Alexander Skornyakov <a1212sk@gmail.com>
2019-10-26 18:19:18 +0300

    connect to firebase

--- app/src/main/java/alexander/skornyakov/yourwords/MainActivity.kt
+++ app/src/main/java/alexander/skornyakov/yourwords/MainActivity.kt
@@ -3,15 +3,20 @@ package alexander.skornyakov.yourwords
 import alexander.skornyakov.yourwords.databinding.MainActivityBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        Log.d("SS----","firebase must be initialized...")
         val binding = DataBindingUtil.inflate<MainActivityBinding>(
             layoutInflater,
             R.layout.main_activity,
