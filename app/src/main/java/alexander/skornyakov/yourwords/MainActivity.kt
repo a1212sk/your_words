68d2e82dcda6e51a33cfa61cf2830b39ec0b28ee
Alexander Skornyakov <a1212sk@gmail.com>
2019-10-27 21:11:12 +0300

    nav drawer

--- app/src/main/java/alexander/skornyakov/yourwords/MainActivity.kt
+++ app/src/main/java/alexander/skornyakov/yourwords/MainActivity.kt
@@ -2,26 +2,42 @@ package alexander.skornyakov.yourwords
 
 import alexander.skornyakov.yourwords.databinding.MainActivityBinding
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.main_activity.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var firebaseAnalytics: FirebaseAnalytics
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         firebaseAnalytics = FirebaseAnalytics.getInstance(this)
         Log.d("SS----","firebase must be initialized...")
+        mAuth = FirebaseAuth.getInstance()
 
         val binding = DataBindingUtil.inflate<MainActivityBinding>(
             layoutInflater,
@@ -32,14 +48,23 @@ class MainActivity : AppCompatActivity() {
 
         auth()
 
+
+
     }
 
+
     private fun auth() {
-        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
-        startActivityForResult(AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build(),RC_SIGN_IN)
+        val user = mAuth.currentUser
+        if(user==null){
+            val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+            startActivityForResult(AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),RC_SIGN_IN)
+        }
+        else{
+            Toast.makeText(applicationContext, user.displayName, Toast.LENGTH_LONG ).show()
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -51,15 +76,14 @@ class MainActivity : AppCompatActivity() {
             if (resultCode == Activity.RESULT_OK) {
                 // Successfully signed in
                 val user = FirebaseAuth.getInstance().currentUser
-                // ...
+
             } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
+                auth()
             }
         }
     }
+
+
 
     companion object {
         private const val RC_SIGN_IN = 123
