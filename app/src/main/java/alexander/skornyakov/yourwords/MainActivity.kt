84afb91244fb7354e7476947f4dc613d60785d14
Alexander Skornyakov <a1212sk@gmail.com>
2019-10-28 13:23:14 +0300

    ViewModel for Main Activity

--- app/src/main/java/alexander/skornyakov/yourwords/MainActivity.kt
+++ app/src/main/java/alexander/skornyakov/yourwords/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -24,10 +25,13 @@ import com.google.android.material.navigation.NavigationView
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.main_activity.*
+import kotlinx.android.synthetic.main.nav_header.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var vm: MainViewModel
 
     private lateinit var firebaseAnalytics: FirebaseAnalytics
     private lateinit var mAuth: FirebaseAuth
@@ -44,15 +48,24 @@ class MainActivity : AppCompatActivity() {
             R.layout.main_activity,
             container,
             false)
+
+        val vm = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        binding.mainViewModel = vm
+        //TODO vm username and email
         setContentView(binding.root)
 
         auth()
 
+        setTitlebar()
+
+
+    }
+
+    private fun setTitlebar() {
         setSupportActionBar(toolbar)
         val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(navController.graph,drawer)
+        val appBarConfiguration = AppBarConfiguration(navController.graph, drawer)
         toolbar.setupWithNavController(navController, appBarConfiguration)
-
     }
 
 
---
+++ app/src/main/java/alexander/skornyakov/yourwords/MainViewModel.kt
@@ -0,0 +1,8 @@
+package alexander.skornyakov.yourwords
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(app: Application): AndroidViewModel(app) {
+
+}
