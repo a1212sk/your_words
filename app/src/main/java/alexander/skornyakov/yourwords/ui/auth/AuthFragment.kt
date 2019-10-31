4a9ee8921eb4ceaa9206b80b347cb28cf853e25b
Alexander Skornyakov <a1212sk@gmail.com>
2019-10-31 10:03:41 +0300

    refactoring authFragment

--- app/src/main/java/alexander/skornyakov/yourwords/ui/auth/AuthFragment.kt
+++ app/src/main/java/alexander/skornyakov/yourwords/ui/auth/AuthFragment.kt
@@ -8,19 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import alexander.skornyakov.yourwords.R
 import alexander.skornyakov.yourwords.databinding.AuthFragmentBinding
-import alexander.skornyakov.yourwords.ui.main.MainActivity
 import alexander.skornyakov.yourwords.ui.main.MainViewModel
 import alexander.skornyakov.yourwords.ui.main.MainViewModelFactory
 import alexander.skornyakov.yourwords.util.Utils
-import android.app.Activity
-import android.app.AlertDialog
 import android.app.Application
-import android.content.Intent
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 
 import kotlinx.coroutines.*
@@ -29,19 +23,11 @@ import java.net.UnknownHostException
 
 import android.net.ConnectivityManager
 import android.content.Context
-import android.content.Context.INPUT_METHOD_SERVICE
 import android.net.NetworkInfo
-import android.view.inputmethod.InputMethodManager
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.Observer
 
 
 class AuthFragment : Fragment() {
-
-    companion object {
-        //fun newInstance() = AuthFragment()
-        private const val RC_SIGN_IN = 123
-    }
 
     private lateinit var viewModel: AuthViewModel
     private lateinit var mAuth: FirebaseAuth
@@ -53,108 +39,69 @@ class AuthFragment : Fragment() {
     ): View? {
 
         val app: Application = requireNotNull(this.activity).application
+        mAuth = FirebaseAuth.getInstance()
 
         //mainViewModel init
         val factory = MainViewModelFactory(app)
         mainViewModel = ViewModelProviders.of(activity!!,factory)[MainViewModel::class.java]
 
-        mAuth = FirebaseAuth.getInstance()
-        val binding = DataBindingUtil.inflate<AuthFragmentBinding>(inflater,R.layout.auth_fragment,container,false)
+
+        //authViewModel and layout init
         val authViewModelFactory = AuthViewModelFactory(mAuth,app)
         viewModel = ViewModelProviders.of(this, authViewModelFactory).get(AuthViewModel::class.java)
+        val binding = DataBindingUtil.inflate<AuthFragmentBinding>(inflater, R.layout.auth_fragment, container,false)
         binding.authViewModel = viewModel
         binding.lifecycleOwner = this
 
+        //Sign in button clicked
         viewModel.signinAction.observe(this, Observer{
             GlobalScope.launch(Dispatchers.Main) {
                 if(it==true){
-                    val email = viewModel._email_edit.value
-                    val password = viewModel._password_edit.value
-                    if(!email.isNullOrEmpty() && !password.isNullOrEmpty()) {
-                        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
-                            if (it.isSuccessful) {
-                                Utils.hideKeyboard(activity!!)
-                               // val user = mAuth.currentUser
-                                mainViewModel.showTitlebar()
-//                                Toast.makeText(context, user?.displayName, Toast.LENGTH_LONG).show()
-                                //findNavController().popBackStack(R.id.setsFragment,true)
-                                findNavController().navigate(AuthFragmentDirections.actionAuthFragmentToSetsFragment())
-                                viewModel.completeSigninAction()
-
-                            } else {
-                                Toast.makeText(
-                                    context,
-                                    "Wrong password or username!",
-                                    Toast.LENGTH_LONG
-                                ).show()
-                            }
-                        }
+                    if(isInternetAvailable()) {
+                        auth()
+                        viewModel.completeSigninAction()
                     }
-                   // auth()
-                   // viewModel.completeSigninAction()
+                    else{
+                        Toast.makeText(context,"Check your internet connection!",Toast.LENGTH_LONG).show()
+                    }
                 }
             }
         })
+
         return binding.root
     }
 
+    private fun auth() {
+        val email = viewModel._email_edit.value
+        val password = viewModel._password_edit.value
+        if (!email.isNullOrEmpty() && !password.isNullOrEmpty()) {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Utils.hideKeyboard(activity!!)
+                    mainViewModel.showTitlebar()
+                    findNavController().navigate(AuthFragmentDirections.actionAuthFragmentToSetsFragment())
+                } else {
+                    Toast.makeText(context, "Wrong password or username!", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
 
-//    private fun auth() {
-//
-//        if( !isNetworkAvailable() || !isInternetAvailable()){
-//            AlertDialog.Builder(context).apply {
-//                setTitle("Internet required!")
-//                setMessage("You need Internet to sign into this application!")
-//            }.create().show()
-//           return
-//        }
-//
-//        val user = mAuth.currentUser
-//        if(user==null){
-//            val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
-//            startActivityForResult(
-//                AuthUI.getInstance()
-//                    .createSignInIntentBuilder()
-//                    .setAvailableProviders(providers)
-//                    .build(), RC_SIGN_IN
-//            )
-//        }
-//        else{
-//            Toast.makeText(context, user.displayName, Toast.LENGTH_LONG ).show()
-//            findNavController().navigate(R.id.action_authFragment_to_setsFragment)
-//        }
-//
-//    }
-
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//
-//        if (requestCode == RC_SIGN_IN) {
-//            val response = IdpResponse.fromResultIntent(data)
-//
-//            if (resultCode == Activity.RESULT_OK) {
-//                // Successfully signed in
-//                val user = FirebaseAuth.getInstance().currentUser
-//                viewModel.changeUsername(user?.displayName)
-//                findNavController().navigate(R.id.action_authFragment_to_setsFragment)
-//            } else {
-//                auth()
-//            }
-//        }
-//    }
-
-    //TODO check internet
     private fun isNetworkAvailable(): Boolean {
         val cm = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
-        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
-        return isConnected
+        return activeNetwork?.isConnectedOrConnecting == true
     }
 
     private fun isInternetAvailable(): Boolean = runBlocking {
         try {
-            val address = withContext(Dispatchers.IO){ InetAddress.getByName("www.google.com")}
-            !address.equals("")
+            if(isNetworkAvailable()) {
+                val address = withContext(Dispatchers.IO) { InetAddress.getByName("www.google.com") }
+                !address.equals("")
+            }
+            else{
+                false
+            }
         } catch (e: UnknownHostException) {
             false
         }
